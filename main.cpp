d1f40af4942e6aa9221e253bdc85fbb1655cdd1e
pcuji <pcuji@hawk.iit.edu>
2021-10-02 19:05:51 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  contains_duplicate
+//
+//  Created by Pedro Jose Cuji on 9/20/17.
+//  Copyright © 2017 Pedro Jose Cuji. All rights reserved.
+//
+
+#include <iostream>
+#include <list>
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // declring list
+    std::list <int> A;
+    std::list <int> B;
+    A.insert(1,2.3,4);
+    //B.insert(3,22,22,3,2);
+
+    std::cout << "hello World" << "\n";
+    return 0;
+}
